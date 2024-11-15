64b10bc4fa5aab5ba2d6be2a8d2097dae9596350
saobernardo <bernardo.pinheiro@hotmail.com>
2024-11-14 21:08:43 -0300

    Alterado ordem de linhas e adiiconado initializer

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ def run():
   print("Aguarde...")
 
   download_speed = st.download() / 1_000_000
+  print(f"Download Speed: {download_speed:.2f} Mpbs")
+
   upload_speed = st.upload() / 1_000_000
+  print(f"Upload Speed: {upload_speed:.2f} Mbps")
 
   ping = st.results.ping
-
-  print(f"Download Speed: {download_speed:.2f} Mpbs")
-  print(f"Upload Speed: {upload_speed:.2f} Mbps")
   print(f"Ping: {ping:.2f} ms")
 
 if __name__ == "__main__":
